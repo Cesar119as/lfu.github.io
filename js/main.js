0f6c4691c68c495405ea91dafed3c0e22e6b6ce0
Cesar119as <59237660+Cesar119as@users.noreply.github.com>
2025-06-10 10:30:09 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-fetch('paginas/inicio.html')
+fetch('inicio.html')
   .then(response => response.text())
   .then(html => {
     document.getElementById('content').innerHTML = html;
